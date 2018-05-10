325fbafd399fe257b84be9d864adc937698c7673
kimchances <kimchances@163.com>
2018-05-10 12:39:55 +0800

    //user.js

---
+++ assets/js/user.js
@@ -0,0 +1,16 @@
+
+(function($) {
+    $(function() {
+
+        var	$window = $(window);
+
+        $window.on('load', function() {
+            /*
+            * 跑数据,个人信息
+            * 跑数据,分类信息
+            * 跑数据,项目列表
+            * */
+        });
+    });
+
+})(jQuery);

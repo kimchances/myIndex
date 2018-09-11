b032a354bf02ebd11802b280a77acb5b5eaaa37d
kimchances <kimchances@163.com>
2018-09-11 11:19:15 +0800

    访问者优化

--- node&nginx [ 服务器渲染+Ajax请求 ]/router.js
+++ node&nginx [ 服务器渲染+Ajax请求 ]/router.js
@@ -1,26 +1,45 @@
 var express = require('express')
 var mysqlSucker = require('./models/mysql-connector')
-// var md5 = require('blueimp-md5')
+var os = require('os');
+var ifaces = os.networkInterfaces();
 var router = express.Router()
 
 router.get('/', function (req, res) {
     res.render('index.html');
-    //查询有没有本身的记录
-    let sqlStatement = `SELECT GuestIpAddress FROM Idx_Guest_Logs WHERE GuestIpAddress='${req.ip}'`;
-    mysqlSucker.query(sqlStatement, [], (results, fields) => {
-        if (results.length) {
-            //更新记录时间
-            let sqlStatement = `UPDATE Idx_Guest_Logs SET GuestTime = NOW() WHERE GuestIpAddress='${req.ip}'`;
-            mysqlSucker.query(sqlStatement, [], (results, fields) => {
-                console.info(`更新了访问时间`)
-            });
-        } else {
-            //写入记录信息
-            let sqlStatement = `INSERT INTO Idx_Guest_Logs (GuestIpAddress,GuestName,GuestTime) VALUES ('${req.ip}','${req.hostname}',NOW())`;
-            mysqlSucker.query(sqlStatement, [], (results, fields) => {
-                console.info(`写入了访问记录`)
-            });
-        }
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                console.log(ifname + ':' + alias, iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                //查询有没有本身的记录
+                    let sqlStatement = `SELECT GuestIpAddress FROM Idx_Guest_Logs WHERE GuestIpAddress='${iface.address}'`;
+                    mysqlSucker.query(sqlStatement, [], (results, fields) => {
+                        if (results.length) {
+                            //更新记录时间
+                            let sqlStatement = `UPDATE Idx_Guest_Logs SET GuestTime = NOW() WHERE GuestIpAddress='${iface.address}'`;
+                            mysqlSucker.query(sqlStatement, [], (results, fields) => {
+                                console.info(`更新了访问时间`)
+                            });
+                        } else {
+                            //写入记录信息
+                            let sqlStatement = `INSERT INTO Idx_Guest_Logs (GuestIpAddress,GuestName,GuestTime) VALUES ('${iface.address}','${ifname}',NOW())`;
+                            mysqlSucker.query(sqlStatement, [], (results, fields) => {
+                                console.info(`写入了访问记录`)
+                            });
+                        }
+                    });
+                console.log(ifname, iface.address);
+            }
+            ++alias;
+        });
     });
 })
 
@@ -34,7 +53,6 @@ router.post('/getUser', function (req, res, next) {
     let sqlStatement = 'SELECT Idx_Info.Idx_If_name AS uName,  Idx_Info.Idx_If_content AS uContent,  Idx_Info.Idx_Pj_Title AS pTitle,  Idx_Info.Idx_Pj_Content AS pContent,  Idx_Info.Idx_If_headImg AS uHeadImg,  Idx_Info.Idx_Wb_title AS wTitle,  Idx_Info.Idx_Wb_nav AS wNav FROM Idx_Info';
     mysqlSucker.query(sqlStatement, [], function (results, fields) {
         res.end(JSON.stringify(results));
-        next();
     });
 });
 //获取分类
@@ -43,7 +61,6 @@ router.post('/getCategory', function (req, res, next) {
     let sqlStatement = 'SELECT Idx_Category.Idx_Ctgr_Name AS cName,  Idx_Category.Idx_Ctgr_NameEn AS cNameEn,  Idx_Category.Idx_Ctgr_Content AS cContent,  Idx_Category.Idx_Ctgr_Url AS cUrl,  Idx_Category.Idx_Ctgr_Font AS cFont,  Idx_Category.Idx_Ctgr_JumpEnable AS jump FROM Idx_Category';
     mysqlSucker.query(sqlStatement, [], function (results, fields) {
         res.end(JSON.stringify(results));
-        next();
     });
 });
 //获取项目列表
@@ -52,7 +69,6 @@ router.post('/getProjects', function (req, res, next) {
     let sqlStatement = '  SELECT Idx_Projects.id AS pId,  Idx_Projects.Idx_Pro_Name AS pName,  Idx_Projects.Idx_Pro_Pics AS pPics,  Idx_Projects.Idx_Pro_URL AS pUrl,  Idx_Projects.Idx_Pro_Content AS pContent,  Idx_Projects.Idx_Pro_JumpEnable AS jump FROM Idx_Projects';
     mysqlSucker.query(sqlStatement, [], function (results, fields) {
         res.end(JSON.stringify(results));
-        next();
     });
 });
 
@@ -67,7 +83,6 @@ router.post('/getProjectDetail', function (req, res, next) {
         let sqlStatement = `SELECT Idx_Pro_C_Html AS myAreaContent FROM Idx_Projects AS pj,  Idx_Projects_Detail AS pd WHERE pj.id = pd.Idx_Pro_C_id AND pj.id =${reqData.projectId}`;
         mysqlSucker.query(sqlStatement, [], function (results, fields) {
             res.end(JSON.stringify(results));
-            next();
         });
     } else {
         res.end('<h1>NULL</h1>>');

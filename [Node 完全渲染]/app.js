c178c5fd8bfec41223bf672aa9a626e5a1b297ae
kimchances <kimchances@163.com>
2019-06-14 11:03:23 +0800

    NEW

---
+++ [Node 完全渲染]/app.js
@@ -0,0 +1,50 @@
+﻿var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var router = require('./router')
+
+var app = express()
+
+app.use('/public/', express.static(path.join(__dirname, './public/')))
+app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
+
+app.set('views', path.join(__dirname, './views/')) // 默认就是 ./views 目录
+app.engine('.html', require('ejs').__express);  
+app.set('view engine', 'html'); 
+
+// 配置解析表单 POST 请求体插件（注意：一定要在 app.use(router) 之前 ）
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(session({
+  // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
+  // 目的是为了增加安全性，防止客户端恶意伪造
+  secret: 'kimchan',
+  resave: false,
+  saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
+}))
+
+// 把路由挂载到 app 中
+app.use(router)
+
+// 配置一个处理 404 的中间件
+app.use(function (req, res) {
+  res.render('404.html')
+})
+
+// 配置一个全局错误处理中间件
+app.use(function (err, req, res, next) {
+  res.status(500).json({
+    err_code: 500,
+    message: err.message
+  })
+})
+
+app.listen(5000, function () {
+  console.log('running...')
+})
---
+++ [Node 完全渲染]/router.js
@@ -0,0 +1,22 @@
+﻿var express = require('express')
+var mysqlSucker = require('./models/mysql-connector')
+var router = express.Router()
+
+router.get('/', function (req, res) {
+    let lastData = {};
+    let sqlStatement = 'SELECT Idx_Info.Idx_If_name AS uName,  Idx_Info.Idx_If_content AS uContent,  Idx_Info.Idx_Pj_Title AS pTitle,  Idx_Info.Idx_Pj_Content AS pContent,  Idx_Info.Idx_If_headImg AS uHeadImg,  Idx_Info.Idx_Wb_title AS wTitle,  Idx_Info.Idx_Wb_nav AS wNav FROM Idx_Info';
+    mysqlSucker.query(sqlStatement, [], function (infoData, fields) {
+        let sqlStatement = 'SELECT Idx_Category.Idx_Ctgr_Name AS cName,  Idx_Category.Idx_Ctgr_NameEn AS cNameEn,  Idx_Category.Idx_Ctgr_Content AS cContent,  Idx_Category.Idx_Ctgr_Url AS cUrl,  Idx_Category.Idx_Ctgr_Font AS cFont,  Idx_Category.Idx_Ctgr_JumpEnable AS jump FROM Idx_Category';
+        mysqlSucker.query(sqlStatement, [], function (categoryData, fields) {
+            let sqlStatement = '  SELECT Idx_Projects.id AS pId,  Idx_Projects.Idx_Pro_Name AS pName,  Idx_Projects.Idx_Pro_Pics AS pPics,  Idx_Projects.Idx_Pro_URL AS pUrl,  Idx_Projects.Idx_Pro_Content AS pContent,  Idx_Projects.Idx_Pro_JumpEnable AS jump FROM Idx_Projects';
+            mysqlSucker.query(sqlStatement, [], function (projectData, fields) {
+                res.render('index.html', {
+                    "info": JSON.parse(JSON.stringify(infoData)),
+                    "category": JSON.parse(JSON.stringify(categoryData)),
+                    "projects": JSON.parse(JSON.stringify(projectData))
+                });
+            });
+        });
+    });
+});
+module.exports = router
